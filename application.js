102fcc1e1dcf9bc4def794c719e15e1e7c09224e
sid <s.sidd707@gmail.com>
2022-09-15 22:49:09 +0545

    jsonadded

--- application.js
+++ application.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const data={name:'Sid'};
+
 
 app.get("/", function(req,res){
     res.send("Welcome to my very first REST API Demo.")
 })
 app.get("/p",function(req,res){
-    res.send("Hello FOOL!");
+    res.json(data);
 });
 
 
