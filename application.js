57217b249d4bb3bc5964c575b41d783be289585d
sid <s.sidd707@gmail.com>
2022-09-15 22:15:52 +0545

    /

--- application.js
+++ application.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/peeps",function(req,res){
+
+app.get("/", function(req,res){
+    res.send("Welcome to my very first REST API Demo.")
+})
+app.get("/p",function(req,res){
     res.send("Hello FOOL!");
 });
 
